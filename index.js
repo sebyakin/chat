9f98b5a5cdf9e8d532a17c37f874b781300b65e4
sebyakin <46461635+sebyakin@users.noreply.github.com>
2020-07-03 20:46:04 +0300

    upload

--- index.js
+++ index.js
@@ -5,21 +5,20 @@ const io      = require('socket.io').listen(server);
 
 app.use(express.static('public'));
 
-
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('user connected');
     
     socket.on('disconnect', () => {
       console.log('user disconnected');
     });
 
+    // отправляет сообщения всем кроме себя
     socket.on('chat message', (msg) => {
       socket.broadcast.emit('chat message', msg);
     });
 
   });
-
   
-server.listen('3000', () => {
-    console.log('Server listening on Port 3000');
+server.listen('3030', () => {
+    console.log('Server listening on Port 3030');
 })
--- public/app.js
+++ public/app.js
@@ -1,30 +1,175 @@
-const messageContainer = document.getElementById('messageContainer');
-const messageForm = document.getElementById('send-container');
-const messageInput = document.getElementById('message-input');
 const socket = io();
 
+const messageContainer = document.getElementById('message-container');
+const messageForm = document.getElementById('send-container');
+const messageInput = document.getElementById('message-input');
+const wrapper = document.querySelector('.wrapper');
+
+const soundOne = new Audio('1.wav');
+const soundTwo = new Audio('2.wav');
+const soundThree = new Audio('3.wav');
+const soundFour = new Audio('4.wav');
+
+// слушаем сокет на чат сообщение, берем оттуда дату и передаем функции которая пишет сообщения
 socket.on('chat message', data => {
     appendMessage(data);
-})
+});
 
-messageForm.addEventListener('submit', e => {
-    // убираем обновление при рефреше
-    e.preventDefault(); 
-    //берем сообщение
-    const message = messageInput.value; 
-    // оставляем свое сообщение 
-    const messageMyWords = document.createElement('li');
-    // messageMyWords.classList.add('myWords')
-    messageMyWords.innerText = message + ' ';
-    messageContainer.append(messageMyWords);
-
-    socket.emit('chat message', message); // передаем его 
-    messageInput.value = ''; // очищаем строку
-})
-
-
+// добавляем сообщения других в чат
 function appendMessage(message){
     const messageElement = document.createElement('span');
+
+        // условия
+        if (message.length < 3)
+        {
+            wrapper.style.backgroundColor = "PaleGreen";
+        }
+
+        if (message.includes("hi"))
+        {
+            wrapper.style.backgroundColor = "pink";
+        }
+
+        if (message.includes("can"))
+        {
+            wrapper.style.backgroundColor = "green";
+        }
+
+        if (message.includes("hello"))
+        {
+            wrapper.style.backgroundColor = "yellow";
+        }
+
+        if (message.includes("awesome"))
+        {
+            wrapper.style.backgroundColor = "blue";
+        }
+
+        if (message.includes("cool"))
+        {
+            wrapper.style.backgroundColor = "LightSteelBlue	";
+        }
+
+        if (message.includes("!"))
+        {
+            soundOne.play();
+            wrapper.classList.add('jump');
+            setTimeout(function(){ 
+                wrapper.classList.remove('jump'); }, 500);
+        }
+
+        if (message.includes("?"))
+        {
+            soundTwo.play();
+            wrapper.classList.add('skew');
+            setTimeout(function(){ 
+                wrapper.classList.remove('skew'); }, 600);
+        }
+        
+        if (message.includes("are"))
+        {
+            wrapper.classList.add('color');
+            setTimeout(function(){ 
+                wrapper.classList.remove('color'); }, 1000);
+        }
+
+        if (message.includes("you"))
+        {
+            wrapper.classList.add('tracking');
+            setTimeout(function(){ 
+                wrapper.classList.remove('tracking'); }, 1000);
+        }
+
+        if (message.includes("weird"))
+        {
+            wrapper.classList.add('weird');
+            setTimeout(function(){ 
+                wrapper.classList.remove('weird'); }, 1000);
+        }
+
+        if (message.includes("ok"))
+        {
+            wrapper.style.backgroundColor = 'brown';
+        }
+
+        if (message.includes("nice"))
+        {
+            wrapper.classList.add('nice');
+            setTimeout(function(){ 
+                wrapper.classList.remove('nice'); }, 300);
+        }
+
+        if (message.includes("no"))
+        {
+            wrapper.classList.add('no');
+            setTimeout(function(){ 
+                wrapper.classList.remove('no'); }, 1000);
+        }
+
+        if (message.includes("."))
+        {
+            wrapper.style.backgroundColor = 'PeachPuff';
+            wrapper.classList.add('scale');
+            setTimeout(function(){ 
+                wrapper.classList.remove('scale'); }, 1000);
+            
+        }
+
+        if (message.includes("chat"))
+        {
+            soundThree.play();
+            wrapper.classList.add('chat');
+            setTimeout(function(){ 
+                wrapper.classList.remove('chat'); }, 1000);
+        }
+
+        if (message.includes("wow"))
+        {
+            wrapper.classList.add('wow');
+            setTimeout(function(){ 
+                wrapper.classList.remove('wow'); }, 700);
+        }
+
+        if (message.includes("words"))
+        {
+            // take every second element
+            for(var i = 0; i < you.length; i += 2) {  
+            you[i].classList.add('blur');
+            }
+        }
+
+        if (message.includes(","))
+        {
+            wrapper.classList.add('comma');
+            setTimeout(function(){ 
+                wrapper.classList.remove('comma'); }, 700);
+        }
+        
+        if (message.includes(":)"))
+        {
+            soundFour.play();
+            wrapper.classList.add('smile');
+            setTimeout(function(){ 
+                wrapper.classList.remove('smile'); }, 400);
+        }
+
     messageElement.innerText = message + ' ';
     messageContainer.append(messageElement);
 }
+
+// отправляем свои сообщения 
+messageForm.addEventListener('submit', e => {
+    // убираем обновление при рефреше
+    e.preventDefault(); 
+    // берем сообщение
+    const message = messageInput.value; 
+    // оставляем свое сообщение 
+    const messageMyWords = document.createElement('span');
+    messageMyWords.classList.add('myWords')
+    messageMyWords.innerText = message + ' ';
+    messageContainer.append(messageMyWords);
+    // передаем его 
+    socket.emit('chat message', message); 
+    // очищаем строку
+    messageInput.value = ''; 
+})
